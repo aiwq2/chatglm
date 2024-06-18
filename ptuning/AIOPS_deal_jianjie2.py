e40e332f0c6caff300847bac5846b20b0b0e05e6
1340282915 <1340282915@qq.com>
2024-06-18 14:11:34 +0800

    将小于1.0的变化值直接忽略

--- ptuning/AIOPS_deal_jianjie2.py
+++ ptuning/AIOPS_deal_jianjie2.py
@@ -29,7 +29,7 @@ node_nums=len(adjcent)
 metrics_name='ACS,AIOS,AWS,Agent_ping,Asm_Free_Tb,Buffers_used,CPU_Used_Pct,CPU_free_pct,CPU_frequency,CPU_idle_pct,CPU_iowait_time,CPU_kernel_number,CPU_number,CPU_pused,CPU_system_time,CPU_user_time,CPU_util_pct,Cache_used,Call_Per_Sec,Cpu_num,DFParaWrite_Per_Sec,DbFile_Used_Pct,DbTime,Disk_avgqu_sz,Disk_await,Disk_io_util,Disk_rd_ios,Disk_rd_kbs,Disk_svctm,Disk_wr_ios,Disk_wr_kbs,Exec_Per_Sec,FS_max_avail,FS_max_util,FS_total_space,FS_used_pct,FS_used_space,Free_disk_space,Free_inodes,Hang,ICMP_ping,Incoming_network_traffic,LFParaWrite_Per_Sec,LFSync_Per_Sec,Logic_Read_Per_Sec,Login_Per_Sec,MEM_Total,MEM_Used,MEM_Used_Pct,MEM_real_util,Memory_available,Memory_available_pct,Memory_free,Memory_total,Memory_used,Memory_used_pct,New_Tbs_Free_Gb,New_Tbs_Used_Pct,Num_of_processes,Num_of_running_processes,On_Off_State,Outgoing_network_traffic,PGA_Used_Pct,PGA_used_total,Page_pi,Page_po,Physical_Read_Per_Sec,Proc_Used_Pct,Proc_User_Used_Pct,Processor_load_15_min,Processor_load_1_min,Processor_load_5_min,Received_errors_packets,Received_packets,Received_queue,Recv_total,Redo_Per_Sec,Row_Lock,SEQ_Used_Pct,SctRead_Per_Sec,Send_total,Sent_errors_packets,Sent_packets,Sent_queue,SeqRead_Per_Sec,Sess_Active,Sess_Connect,Sess_Used_Temp,Sess_Used_Undo,Session_pct,Shared_memory,Swap_used_pct,System_block_queue_length,System_wait_queue_length,TPS_Per_Sec,Tbs_Free_Gb,Tbs_Used_Pct,TempTbs_Pct,Total_Tbs_Size,Total_disk_space,Total_inodes,UndoTbs_Pct,Used_Tbs_Size,Used_disk_space,Used_disk_space_pct,Used_inodes,Used_inodes_pct,User_Commit,Zombie_Process,container_cpu_used,container_fgc,container_fgct,container_mem_used,container_session_used,container_thread_idle,container_thread_running,container_thread_total,container_thread_used_pct,cost,count,proc,ss_total,succ_rate,tnsping_result_time'
 metric_name_list=metrics_name.split(',')
 # prompt_prefix=f'现在微服务中总共有{node_nums}个节点,其中每个节点在某一个timestamp时刻,会存在一共{len(metric_name_list)}个metrics(指标)状态。'
-prompt_prefix=f'微服务中一个节点在某一个timestamp时刻,会存在一共{len(metric_name_list)}个metrics(指标)状态。'
+prompt_prefix=f'微服务中一个节点在某一个timestamp时刻,会存在一共{len(metric_name_list)}个metrics(指标)状态,例如CPU_Used_Pct,CPU_free_pct等。'
 
 
 
@@ -76,23 +76,24 @@ for index,(time,node_metric_timerange,label) in enumerate(list(zip(timestamp,nod
             up_list=[]
             down_list=[]
             for index,(metric_value_now,metric_value_last) in enumerate(zip(metrics_values_list,metrics_last_list)):
-                if math.fabs(metric_value_last-metric_value_now)>1e-5:
+                # 目前这版差值要大于1.0才进行统计
+                if math.fabs(metric_value_last-metric_value_now)>1.0:
                     flag=1
                     diff=metric_value_now-metric_value_last
                     if diff>0:
                         # prompt_diff=f'{metric_name_list[index]}上涨了{diff:.2f},'
                         # prompt_diff=f'metric{index}上涨了{diff:.2f},'
-                        up_list.append(f'metric{index}:{diff:.2f}')
+                        up_list.append(f'{metric_name_list[index]}:{diff:.2f}')
                     else:
                         # prompt_diff=f'{metric_name_list[index]}下降了{-diff:.2f},'
                         # prompt_diff=f'metric{index}下降了{-diff:.2f},'
-                        down_list.append(f'metric{index}:{diff:.2f}')
+                        down_list.append(f'{metric_name_list[index]}:{diff:.2f}')
             
             if flag==0:
-                prompt3+='没有变化。'
+                prompt3+='无较大变化。'
             else:
-                up_value=",".join(up_list) if len(up_list)>0 else '无变化'
-                down_value=",".join(down_list) if len(down_list)>0 else '无变化'
+                up_value=",".join(up_list) if len(up_list)>0 else '无较大变化'
+                down_value=",".join(down_list) if len(down_list)>0 else '无较大变化'
                 prompt_diff=f'上涨指标及其对应的值为:{up_value},下降指标及其对应的值为:{down_value}。'
                 prompt3+=prompt_diff
             prompt23+=prompt3
@@ -103,6 +104,7 @@ for index,(time,node_metric_timerange,label) in enumerate(list(zip(timestamp,nod
         # print(content_summary_dict['content'])
         content_summary_dict['summary']=summary
         nodes_prompt_list.append(content_summary_dict)
+
 
 
 np.random.seed(42)
@@ -126,8 +128,8 @@ with open('AIOPS/jianjie2/dev_jianjie2.json','w',encoding='utf-8') as f:
 
 
 
-# with open('AIOPS/jianjie2/content_example_jianjie2.txt','w',encoding='utf-8') as f:
-#     f.write(nodes_prompt_list[0]['content'])
+with open('AIOPS/jianjie2/content_example_jianjie2.txt','w',encoding='utf-8') as f:
+    f.write(nodes_prompt_list[0]['content'])
 
 
 
