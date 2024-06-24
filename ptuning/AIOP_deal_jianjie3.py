29c92ac2fdf57ecbe3ac32f7f68b122533213374
1340282915 <1340282915@qq.com>
2024-06-24 21:20:19 +0800

    新增jianjie3,里面的数据保证了同一个时间窗口内的数据都是连续的,并且调整了下label的prompt

---
+++ ptuning/AIOP_deal_jianjie3.py
@@ -0,0 +1,133 @@
+import math
+import os
+import json
+import numpy as np
+
+timestamp=np.load('AIOPS/real_timestamp_3.npy') # (2220,)
+labels_timestamp=np.load('AIOPS/real_label_3.npy') # (2220,)
+labels_node=np.load('AIOPS/labels_nodes_3.npy')# (2220, 17)
+nodes=np.load('AIOPS/real_node_3.npy') # (2220, 17, 6, 124)
+adjcent=np.load('AIOPS/total_A.npy') # (17,17)
+
+nodes=nodes.transpose(0,1,3,2) # (2220, 17, 6, 124)
+
+input_dict_file='AIOPS/node_dict.json'
+with open(input_dict_file, 'r') as f:
+    input_dict = json.load(f)
+
+value_name_dict={}
+for name,value in input_dict.items():
+    value_name_dict[value]=name
+
+print(labels_timestamp.shape)
+print(labels_node.shape)
+print(timestamp.shape)
+print(nodes.shape) # (2220, 17, 6, 124)
+
+
+
+
+system_prompt="""你是一位优秀的网络运维工程师,能够根据一定时间步下的网络节点的metrics(指标)情况以及节点之间
+的邻接关系判断网络中是否存在异常。现在我将要给出这个微服务中的节点之间的邻接关系以及每一个节点在一定时间步下的metric(指标)情况，
+请你输出此时网络中是否存在异常,存在异常为1,不存在异常为0,你只需要输出0或者1即可。
+"""
+
+node_nums=len(adjcent)
+metrics_name='ACS,AIOS,AWS,Agent_ping,Asm_Free_Tb,Buffers_used,CPU_Used_Pct,CPU_free_pct,CPU_frequency,CPU_idle_pct,CPU_iowait_time,CPU_kernel_number,CPU_number,CPU_pused,CPU_system_time,CPU_user_time,CPU_util_pct,Cache_used,Call_Per_Sec,Cpu_num,DFParaWrite_Per_Sec,DbFile_Used_Pct,DbTime,Disk_avgqu_sz,Disk_await,Disk_io_util,Disk_rd_ios,Disk_rd_kbs,Disk_svctm,Disk_wr_ios,Disk_wr_kbs,Exec_Per_Sec,FS_max_avail,FS_max_util,FS_total_space,FS_used_pct,FS_used_space,Free_disk_space,Free_inodes,Hang,ICMP_ping,Incoming_network_traffic,LFParaWrite_Per_Sec,LFSync_Per_Sec,Logic_Read_Per_Sec,Login_Per_Sec,MEM_Total,MEM_Used,MEM_Used_Pct,MEM_real_util,Memory_available,Memory_available_pct,Memory_free,Memory_total,Memory_used,Memory_used_pct,New_Tbs_Free_Gb,New_Tbs_Used_Pct,Num_of_processes,Num_of_running_processes,On_Off_State,Outgoing_network_traffic,PGA_Used_Pct,PGA_used_total,Page_pi,Page_po,Physical_Read_Per_Sec,Proc_Used_Pct,Proc_User_Used_Pct,Processor_load_15_min,Processor_load_1_min,Processor_load_5_min,Received_errors_packets,Received_packets,Received_queue,Recv_total,Redo_Per_Sec,Row_Lock,SEQ_Used_Pct,SctRead_Per_Sec,Send_total,Sent_errors_packets,Sent_packets,Sent_queue,SeqRead_Per_Sec,Sess_Active,Sess_Connect,Sess_Used_Temp,Sess_Used_Undo,Session_pct,Shared_memory,Swap_used_pct,System_block_queue_length,System_wait_queue_length,TPS_Per_Sec,Tbs_Free_Gb,Tbs_Used_Pct,TempTbs_Pct,Total_Tbs_Size,Total_disk_space,Total_inodes,UndoTbs_Pct,Used_Tbs_Size,Used_disk_space,Used_disk_space_pct,Used_inodes,Used_inodes_pct,User_Commit,Zombie_Process,container_cpu_used,container_fgc,container_fgct,container_mem_used,container_session_used,container_thread_idle,container_thread_running,container_thread_total,container_thread_used_pct,cost,count,proc,ss_total,succ_rate,tnsping_result_time'
+metric_name_list=metrics_name.split(',')
+# prompt_prefix=f'现在微服务中总共有{node_nums}个节点,其中每个节点在某一个timestamp时刻,会存在一共{len(metric_name_list)}个metrics(指标)状态。'
+prompt_prefix=f'微服务中一个节点在某一个timestamp时刻,会存在一共{len(metric_name_list)}个metrics(指标)状态,例如CPU_Used_Pct,CPU_free_pct等。'
+
+
+# prompt_metric=f"""我们将其记录为metric0,metric1,...一直到metric{len(metric_name_list)-1}。这些metric可能为例如Buffers_used,CPU_free_pct等,值为保留两位小数后的浮点数。接下来我将给出每个不同节点在三个等间隔timestamp所形成的时间窗口下,所对应的每个timestamp相比于上一个timestamp的metrics变化值,如果没有指出某个metric则说明这个metric在这个timestamp中相比于上一个timestamp没有变化,我们将用这个时间窗口的metric变化情况推断该窗口下是否存在异常。"""
+prompt_metric=f"""接下来我将给出某个节点在6个等间隔timestamp所形成的时间窗口下,所对应的每个timestamp相比于上一个timestamp存在变化的metrics变化值。"""
+prompt_prefix+=prompt_metric
+# print(prompt_prefix)
+
+nodes_prompt_list=[]
+for index,(time,node_metric_timerange,label) in enumerate(list(zip(timestamp,nodes,labels_node))): # nodes的shape为 (1534, 17, 6, 124),labels_node的shape为（1534，17）
+    prompt_element=''
+    prompt1=f'在时间窗口{time},共包含{len(node_metric_timerange[0])}个时刻:'
+    prompt_element+=prompt1
+    for i in range(len(node_metric_timerange)):
+        node_name=value_name_dict[i]
+        prompt23=''
+        prompt2=f'节点为{node_name}时,'
+        prompt23+=prompt2
+        # prompt_element+=prompt2
+        for j in range(len(node_metric_timerange[i])):
+            # 跳过第一个
+            if j==0:
+                continue
+        
+            metrics_values_list=node_metric_timerange[i][j]
+            metrics_values_list=np.round(metrics_values_list,2)
+            metrics_last_list=node_metric_timerange[i][j-1]
+            metrics_last_list=np.round(metrics_last_list,2)
+            # prompt3=f'在timestamp为{timestamp_now}下相较于{timestamp_now-60},'
+            prompt3=f'第{j+1}时刻相较于上一时刻,'
+            flag=0
+            up_list=[]
+            down_list=[]
+            for index,(metric_value_now,metric_value_last) in enumerate(zip(metrics_values_list,metrics_last_list)):
+                # 目前这版差值要大于1.0才进行统计
+                if math.fabs(metric_value_last-metric_value_now)>1.0:
+                    flag=1
+                    diff=metric_value_now-metric_value_last
+                    if diff>0:
+                        # prompt_diff=f'{metric_name_list[index]}上涨了{diff:.2f},'
+                        # prompt_diff=f'metric{index}上涨了{diff:.2f},'
+                        up_list.append(f'{metric_name_list[index]}:{diff:.2f}')
+                    else:
+                        # prompt_diff=f'{metric_name_list[index]}下降了{-diff:.2f},'
+                        # prompt_diff=f'metric{index}下降了{-diff:.2f},'
+                        down_list.append(f'{metric_name_list[index]}:{diff:.2f}')
+            
+            if flag==0:
+                prompt3+='无较大变化。'
+            else:
+                up_value=",".join(up_list) if len(up_list)>0 else '无较大变化'
+                down_value=",".join(down_list) if len(down_list)>0 else '无较大变化'
+                prompt_diff=f'上涨指标及其对应的值为:{up_value},下降指标及其对应的值为:{down_value}。'
+                prompt3+=prompt_diff
+            prompt23+=prompt3
+
+        content_summary_dict={}
+        content_summary_dict['content']=prompt_prefix+prompt_element+prompt23
+        summary='该节点存在异常(1)' if label[i]==1 else '该节点为正常状态(0)'
+
+        content_summary_dict['summary']=summary
+        # if label[i]==1:
+        #     print(content_summary_dict['content'])
+        #     print(summary)
+        nodes_prompt_list.append(content_summary_dict)
+
+
+
+# np.random.seed(42)
+# np.random.shuffle(nodes_prompt_list)
+print(len(nodes_prompt_list))
+train_num_split_point=int(len(timestamp)*0.8*len(value_name_dict))
+
+train_nodes_prompt_list=nodes_prompt_list[:train_num_split_point]
+print(len(train_nodes_prompt_list))
+dev_nodse_prompt_list=nodes_prompt_list[train_num_split_point:]
+print(len(dev_nodse_prompt_list))
+
+
+with open('AIOPS/jianjie3/train_jianjie3.json','w',encoding='utf-8') as f:
+    for prompt_finish in train_nodes_prompt_list:
+        f.write(json.dumps(prompt_finish,ensure_ascii=False)+'\n')
+
+with open('AIOPS/jianjie3/dev_jianjie3.json','w',encoding='utf-8') as f:
+    for prompt_finish in dev_nodse_prompt_list:
+        f.write(json.dumps(prompt_finish,ensure_ascii=False)+'\n')
+
+
+
+with open('AIOPS/jianjie3/content_example_jianjie3.txt','w',encoding='utf-8') as f:
+    f.write(nodes_prompt_list[0]['content'])
+
+
+
+
--- test.py
+++ test.py
@@ -1,11 +1,13 @@
 from transformers import AutoTokenizer, AutoModel,AutoConfig
 import os
 import torch
+import json
+from tqdm import tqdm
+from sklearn.metrics import accuracy_score,precision_score,recall_score,classification_report,roc_auc_score
 
 tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True)
 config = AutoConfig.from_pretrained("THUDM/chatglm-6b", trust_remote_code=True, pre_seq_len=128)
 model = AutoModel.from_pretrained("THUDM/chatglm-6b", config=config,trust_remote_code=True)
-chat_input='微服务中一个节点在某一个timestamp时刻,会存在一共124个metrics(指标)状态,例如CPU_Used_Pct,CPU_free_pct等。接下来我将给出每个节点在三个等间隔timestamp所形成的时间窗口下,所对应的每个timestamp相比于上一个timestamp存在变化的metrics变化值。第1408个时间窗口,共包含3个时刻:节点为os_018时,第2时刻相较于上一时刻,上涨指标及其对应的值为:Cache_used:1.22,Disk_svctm:11.21,Memory_free:1.07,下降指标及其对应的值为:Received_packets:-1.13,Sent_packets:-1.42。第3时刻相较于上一时刻,上涨指标及其对应的值为:CPU_iowait_time:7.47,Disk_avgqu_sz:4.64,Disk_await:1.33,Disk_io_util:1.01,Disk_wr_ios:12.50,下降指标及其对应的值为:Cache_used:-1.22,Disk_svctm:-6.11,Memory_free:-1.07。'
 CHECKPOINT_PATH='ptuning/output/aiops-chatglm-6b-pt-128-2e-2/checkpoint-1000'
 
 prefix_state_dict = torch.load(os.path.join(CHECKPOINT_PATH, "pytorch_model.bin"))
@@ -19,8 +21,49 @@ model.transformer.prefix_encoder.load_state_dict(new_prefix_state_dict)
 model=model.quantize(4).half().cuda()
 model.transformer.prefix_encoder.float()
 model = model.eval()
-response, history = model.chat(tokenizer, chat_input, history=[])
-print(response)
+labels_01=[]
+pred=[]
+with open(r'D:\计算所研究生学习\毕业设计\renrui code\my_code\ChatGLM-6B\ptuning\AIOPS\jianjie3\dev_jianjie3.json','w') as f:
+    for line in tqdm(f.readlines()):
+        line=line.strip()
+        chat_input_dict=json.loads(line)
+        label=chat_input_dict['summary']
+        if '0' in label:
+            labels_01.append(0)
+        elif '1' in label:
+            labels_01.append(1)
+        else:
+            print('labels存在问题,不属于0,1的分类')
+        # chat_input='微服务中一个节点在某一个timestamp时刻,会存在一共124个metrics(指标)状态,例如CPU_Used_Pct,CPU_free_pct等。接下来我将给出每个节点在三个等间隔timestamp所形成的时间窗口下,所对应的每个timestamp相比于上一个timestamp存在变化的metrics变化值。第1408个时间窗口,共包含3个时刻:节点为os_018时,第2时刻相较于上一时刻,上涨指标及其对应的值为:Cache_used:1.22,Disk_svctm:11.21,Memory_free:1.07,下降指标及其对应的值为:Received_packets:-1.13,Sent_packets:-1.42。第3时刻相较于上一时刻,上涨指标及其对应的值为:CPU_iowait_time:7.47,Disk_avgqu_sz:4.64,Disk_await:1.33,Disk_io_util:1.01,Disk_wr_ios:12.50,下降指标及其对应的值为:Cache_used:-1.22,Disk_svctm:-6.11,Memory_free:-1.07。'
+        chat_input=chat_input_dict['content']
+        response, history = model.chat(tokenizer, chat_input, history=[])
+        if '1' not in response and '0' not in response:
+            print('response error,没有获得正确的答案')
+        elif '1' in response and '0' in response:
+            print('1和0都存在于答案当中,因此答案存在一定的问题')
+        elif '0' in response:
+            pred.append(0)
+        else:
+            pred.append(1)
+
+
+accuracy=accuracy_score(labels_01,pred)
+precision=precision_score(labels_01,pred)
+recall=recall_score(labels_01,pred)
+report=classification_report(labels_01,pred)
+
+with open('result.txt','r') as f:
+    f.write(f'accuracy:{accuracy:.2f}\n')
+    f.write(f'precision:{precision:.2f}\n')
+    f.write(f'recall:{recall:.2f}\n')
+
+print(f'accuracy:{accuracy:.2f}')
+print(f'precision:{precision:.2f}')
+print(f'recall:{recall:.2f}')
+print('report:')
+print(report)
+
+
 
 
 # 原始输入
